a9f729ea914e6da41a91436ab2c1ef514407d49c
Berindeiu Sergiu <sergiu.a.berindeiu@gmail.com>
2025-11-15 19:35:39 +0200

    added missing imporst

--- app/src/main/java/com/example/safetytracker/MainActivity.kt
+++ app/src/main/java/com/example/safetytracker/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.safetytracker.ui.screens.ContactsScreenWithData
+import com.example.safetytracker.ui.screens.HomeScreen
 import com.example.safetytracker.ui.theme.SafetyTrackerTheme
 
 class MainActivity : ComponentActivity() {
