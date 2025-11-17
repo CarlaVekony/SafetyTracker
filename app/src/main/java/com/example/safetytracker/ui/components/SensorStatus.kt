0c99d5e764cad01a3f1e40d671eedc3d3edbb9ff
vcarla <carla.vekony@student.upt.ro>
2025-11-17 22:45:03 +0200

    Final changes

--- app/src/main/java/com/example/safetytracker/ui/components/SensorStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.example.safetytracker.ui.components
-
-// TODO: Implement sensor status indicator component
-// TODO: Display real-time status of accelerometer, gyroscope, microphone, GPS
-// TODO: Show visual indicators for active/inactive sensors
-// TODO: Display current sensor readings when active
